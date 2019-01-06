8d8d29c872386ae4069207cffe7e3ef88b85e7b8
Gabriel Caruso <carusogabriel34@gmail.com>
2019-01-06 16:36:07 -0200

    Add Sniff to require the usage of ::class whenever possible
    Resolves #9

---
+++ tests/expected/class-references.php
@@ -0,0 +1,21 @@
+<?php
+
+class Foo
+{
+}
+
+class Bar extends Foo
+{
+    /**
+     * @return string[]
+     */
+    public function names(): iterable
+    {
+        yield self::class;
+        yield self::class;
+        yield static::class;
+        yield get_class(new stdClass());
+        yield parent::class;
+        yield static::class;
+    }
+}
