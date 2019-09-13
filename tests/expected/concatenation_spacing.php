efcc829784908e7cea277a9e5ceb8f093ee1855d
Ravan Scafi <rscafi@leroymerlin.com.br>
2019-09-13 11:17:28 -0300

    Add PSR-12 rules

--- tests/expected/concatenation_spacing.php
+++ tests/expected/concatenation_spacing.php
@@ -1,22 +1,22 @@
 <?php
 
-$string = 'Leroy Merlin'.'bar';
+$string = 'Leroy Merlin' . 'bar';
 
 $foo = 'Leroy Merlin';
-$bar    = 'bar';
-$string   = $foo.$bar;
+$bar = 'bar';
+$string = $foo . $bar;
 
-$string = 'Leroy Merlin'.$bar;
+$string = 'Leroy Merlin' . $bar;
 
-$string = $foo.'bar';
+$string = $foo . 'bar';
 
-$string = $foo.$bar.'even'.'more'.$string;
+$string = $foo . $bar . 'even' . 'more' . $string;
 
 $string .= $foo;
 $string .= $foo;
 $string .= $foo;
 
-$string = $foo.
-    $bar.
-    'all'
-    .$string;
+$string = $foo .
+          $bar .
+          'all'
+          . $string;
