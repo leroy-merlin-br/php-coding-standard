a17da0d6174632353c18fe65af3407e0ab4c6094
Gabriel Gomes <gabrielgomesch@gmail.com>
2021-02-16 16:12:19 -0300

    feat: add tests for type hints

--- tests/input/type-hints.php
+++ tests/input/type-hints.php
@@ -1,1 +1,57 @@
 <?php
+
+namespace Types;
+
+class TypeHint
+{
+    public $data;
+
+    /**
+     * @var string
+     */
+    public $name;
+
+    public int $age;
+
+    /**
+     * @var ?int
+     */
+    public $foo;
+
+    /**
+     * @const float
+     */
+    public const PI = 3.1415;
+
+    public function test()
+    {
+        // Do nothing
+    }
+
+    public function get($id): int
+    {
+        //
+    }
+
+    public function getWithParamTypeHint(int $id): int
+    {
+        return 1;
+    }
+
+    /**
+     * @param int $id
+     * @dataProvider
+     */
+    public function post($id): void
+    {
+        //
+    }
+
+    /**
+     * @return int
+     */
+    public function returnType()
+    {
+        return 2;
+    }
+}
